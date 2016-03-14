b171181bcc1955674d368a132de0d1c99696467b
Simon Koelsch <simon.koelsch@innoq.com>
2016-03-14 14:25:34 +0100

    added datacenters

--- join.sh
+++ join.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 docker exec consul_node-a_1 /bin/consul join consul_node-b_1 consul_node-c_1
+docker exec consul_node-d_1 /bin/consul join consul_node-e_1 consul_node-f_1
+docker exec consul_node-d_1 /bin/consul join -wan consul_node-a_1 consul_node-b_1 consul_node_c_2
+docker exec consul_node-e_1 /bin/consul join -wan consul_node-a_1 consul_node-b_1 consul_node_c_2
+docker exec consul_node-f_1 /bin/consul join -wan consul_node-a_1 consul_node-b_1 consul_node_c_2
 docker exec consul_agent-a_1 /bin/consul join consul_node-a_1
 docker exec consul_agent-b_1 /bin/consul join consul_node-a_1
