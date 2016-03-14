d5039dabb66ee7256eee81707c8be6a265a71176
Simon Koelsch <simon.koelsch@innoq.com>
2016-03-14 13:44:12 +0100

    more agents, more servers

--- join.sh
+++ join.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker exec consul_node-a_1 /bin/consul join consul_node-b_1 consul_node-c_1
-docker exec consul_agent_1 /bin/consul join consul_node-a_1
+docker exec consul_agent-a_1 /bin/consul join consul_node-a_1
+docker exec consul_agent-b_1 /bin/consul join consul_node-a_1
